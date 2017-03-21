fc69b3d1cd47dcec380ba8a86fca16316db4e75b
Chet Corcos <ccorcos@gmail.com>
2017-03-21 01:25:39 -0700

    not working

--- src/reactive.js
+++ src/reactive.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { css } from "glamor";
 
 // Because JavaScript is single threaded, we can use this singleton to keep track of which computation is currently running
 const Tracker = {
@@ -31,16 +32,37 @@ class Computation {
   }
 }
 
+const RenderLoop = {
+  waiting: false,
+  queue: {},
+  run(dep) {
+    this.queue[dep.id] = dep;
+    if (!this.waiting) {
+      this.waiting = true;
+      window.requestAnimationFrame(() => this.flush());
+    }
+  },
+  flush() {
+    Object.keys(this.queue).forEach(id => this.queue[id].run());
+    this.queue = {};
+    this.waiting = false;
+  }
+};
+
 // A Dependency keeps track of computations that depend on some value.
 class Dependency {
-  subscribers = {};
+  constructor() {
+    this.subscribers = {};
+    this.id = id();
+  }
   depend() {
     if (Tracker.current) {
       this.subscribers[Tracker.current.id] = Tracker.current;
     }
   }
   run() {
-    Object.keys(this.subscribers).forEach(id => this.subscribers[id].run());
+    Object.keys(this.subscribers).forEach(id =>
+      RenderLoop.run(this.subscribers[id]));
   }
 }
 
@@ -95,10 +117,6 @@ class ReactiveComponent extends React.PureComponent {
 }
 
 class Counter extends ReactiveComponent {
-  // constructor(props) {
-  //   super(props);
-  // }
-
   store = new ReactiveMap({
     count: 0
   });
@@ -115,4 +133,167 @@ class Counter extends ReactiveComponent {
   };
 }
 
-export default Counter;
+const canvas = css({
+  position: "absolute",
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0
+});
+
+class Canvas extends ReactiveComponent {
+  store = new ReactiveMap({
+    state: "normal",
+    start: undefined,
+    current: undefined,
+    selected: 0,
+    nextId: 0,
+    rects: {}
+  });
+
+  constructor(props) {
+    super(props);
+  }
+
+  mousedown = event => {
+    const point = {
+      x: event.pageX,
+      y: event.pageY
+    };
+    this.store.state = "mousedown";
+    this.store.start = point;
+    this.store.current = point;
+    this.store.selected = this.store.nextId;
+  };
+
+  mousemove = event => {
+    if (this.store.state === "mousedown") {
+      const point = {
+        x: event.pageX,
+        y: event.pageY
+      };
+      this.store.current = point;
+    }
+  };
+
+  position = () => {
+    return new ReactiveMap({
+      top: Math.min(this.store.start.y, this.store.current.y),
+      left: Math.min(this.store.start.x, this.store.current.x),
+      height: Math.abs(this.store.start.y - this.store.current.y),
+      width: Math.abs(this.store.start.x - this.store.current.x)
+    });
+  };
+
+  mouseup = event => {
+    if (this.store.state === "mousedown") {
+      const point = {
+        x: event.pageX,
+        y: event.pageY
+      };
+      this.store.state = "normal";
+      this.store.rects[this.store.nextId] = this.position();
+      this.store.start = undefined;
+      this.store.current = undefined;
+      this.store.nextId++;
+    }
+  };
+
+  mapRects = fn => {
+    return Object.keys(this.store.rects).map(key => {
+      return fn(this.store.rects[key], key);
+    });
+  };
+
+  onSelect = id => {
+    this.store.selected = id;
+  };
+
+  view() {
+    return (
+      <div
+        className={canvas}
+        onMouseDown={this.mousedown}
+        onMouseMove={this.mousemove}
+        onMouseUp={this.mouseup}
+      >
+        {this.mapRects((pos, id) => {
+          return (
+            <Rect
+              key={id}
+              id={id}
+              position={pos}
+              selected={this.store.selected.toString() === id}
+              onClick={this.onSelect}
+            />
+          );
+        })}
+        {this.store.state === "mousedown" &&
+          <Rect
+            id={this.store.nextId}
+            position={this.position()}
+            selected={this.store.selected === this.store.nextId}
+            onClick={this.onSelect}
+          />}
+      </div>
+    );
+  }
+}
+
+const rect = css({
+  borderWidth: 1,
+  borderStyle: "solid",
+  borderColor: "black",
+  backgroundColor: "#cccccc",
+  borderRadius: 3
+});
+
+const selected = css({
+  borderWidth: 4,
+  borderColor: "blue"
+});
+
+class Rect extends ReactiveComponent {
+  onMouseDown = event => {
+    event.preventDefault();
+    event.stopPropagation();
+  };
+
+  onClick = event => {
+    this.props.onClick(this.props.id);
+  };
+
+  view() {
+    return (
+      <div
+        className={css(rect, this.props.selected && selected)}
+        onClick={this.onClick}
+        onMouseDown={this.onMouseDown}
+        style={{
+          position: "absolute",
+          top: this.props.position.top,
+          bottom: this.props.position.bottom,
+          width: this.props.position.width,
+          height: this.props.position.height
+        }}
+      />
+    );
+  }
+}
+
+const style = css({
+  color: "blue"
+});
+
+class Index extends React.PureComponent {
+  render() {
+    return (
+      <div className={style}>
+        Hello World!
+        <Canvas />
+      </div>
+    );
+  }
+}
+
+export default Index;
